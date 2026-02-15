30b4164b4715cca502df4a9816fd7e8a177c5035
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-02-15 19:23:07 -0300

    Configuração geral da views e url além de criar os templates home e serviços junto com seus estilos por enquanto

--- barbearia/models.py
+++ barbearia/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-from django.db import models
-
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
--- barbearia/views.py
+++ barbearia/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Servico
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def lista_servicos(request):
+    servicos = Servico.objects.all()
+    return render(request, 'servicos.html', {'servicos': servicos})
